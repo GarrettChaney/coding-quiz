06e5e9dd12d4c35f3c19922d634a982b597c5516
Garrett Chaney <gbcpursuits@gmail.com>
2022-12-12 18:05:30 +1100

    Added more to base script files.

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,43 @@
-var startContainerElement = document.getElementById("start-container")
-var questionContainerElement = document.getElementById("question-container")
-var startBtn = document.getElementById("start")
-var answerOne = document.getElementById("answer1")
-var answerTwo = document.getElementById("answer2")
-var answerThree = document.getElementById("answer3")
-var answerFour = document.getElementById("answer4")
+// header elements
+let highscoresElement = document.getElementById("highscores");
+let timerElement = document.getElementById("timer");
 
-startBtn.addEventListener("click", startGame)
+// start container elements
+let startContainerElement = document.getElementById("start-container");
+let startButtonElement = document.getElementById("start-button");
+
+// question elements
+let questionContainerElement = document.getElementById("question-container");
+let questionElement = document.getElementById("question");
+
+let shuffledQuestions, currentQuestionIndex;
+
+// answer elements
+let answerButtonElement = document.getElementById("answers");
+
+// start button listener
+startButtonElement.addEventListener("click", startGame)
 
 
 function startGame() {
-    //startTimer()
-    var count = 0
+    console.log("started");
     startContainerElement.classList.add("hidden");
-    questionContainerElement.classList.remove("hidden")
-    nextQuestion()
+    questionContainerElement.classList.remove("hidden");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0
+    console.log(shuffledQuestions)
+    nextQuestion();
+
+}
+
+function showQuestion(question) {
 
 }
 
 function nextQuestion() {
+    console.log("fired nextQuestion()")
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
 
 }
 
